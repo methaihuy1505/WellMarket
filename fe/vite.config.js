ab5333e3abc5ffdcb453b7f88ede1069896dcb69
methaihuy <methaihuy1505@gmail.com>
2025-11-01 18:18:41 +0700

    upd dockerfile

--- fe/vite.config.js
+++ fe/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,       // Cho phép truy cập từ bên ngoài container
+    port: 5173        // Khớp với EXPOSE trong Dockerfile
+  }
 })
