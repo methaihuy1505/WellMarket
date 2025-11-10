5aa060623505c3e29a183ff36466527cc37908b3
unknown <lol78955@gmail.com>
2025-11-10 16:30:09 +0700

    Login and Register - 90%

--- fe/src/components/Login/Login.jsx
+++ fe/src/components/Login/Login.jsx
@@ -1,18 +1,21 @@
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client"; // <<< quan trọng: dùng named import createRoot
 import { RegisterModal } from "./Register";
+import bcrypt from "bcryptjs";
+import axios from "axios";
+
 function LoginModalContent({ onClose }) {
   const [phone, setPhone] = useState("");
-  const [error, setError] = useState("");
+  const [error, setPhoneError] = useState("");
+  const [pswd, setPassword] = useState("");
+  const [errorPwd, setErrorPwd] = useState("");
 
-
-   const handleRegisterClick = () => {
+  const handleRegisterClick = () => {
     onClose(); // đóng modal login
     setTimeout(() => {
       RegisterModal.show(); // mở modal đăng ký
     }, 200);
   };
-
 
   const handleOverlayClick = (e) => {
     if (e.target === e.currentTarget) onClose();
@@ -21,20 +24,44 @@ function LoginModalContent({ onClose }) {
   const validatePhone = (value) => {
     const regex = /^0\d{9}$/;
     if (!value) return "Vui lòng nhập số điện thoại";
-    if (!regex.test(value)) return "Số điện thoại phải gồm 10 chữ số và bắt đầu bằng 0";
+    if (!regex.test(value))
+      return "Số điện thoại phải gồm 10 chữ số và bắt đầu bằng 0";
     return "";
   };
 
   const handleSubmit = () => {
     const err = validatePhone(phone);
+    const saltRound = 5;
     if (err) {
-      setError(err);
+      setPhoneError(err);
       return;
+    } else {
+      setPhoneError("");
     }
-    setError("");
-    const result = { success: true, phone };
-    console.log("Login submit ->", result);
-    // nếu cần gọi API ở đây thì gọi rồi onClose() sau khi có kết quả
+    if (!pswd) {
+      setErrorPwd("Vui lòng nhập mật khẩu");
+      return;
+    } else {
+      setErrorPwd("");
+    }
+    // hash pass
+    const hashedpswd = bcrypt.hashSync(pswd, saltRound);
+    console.log("Phone: ", phone, "Password", hashedpswd);
+    const data = {
+      Phone: phone,
+      Passord: hashedpswd,
+    };
+
+    //gửi = axios
+    axios
+      .post("http://localhost:8000/login", data)
+      .then((res) => {
+        console.log("Server trả về:", res.data);
+      })
+      .catch((err) => {
+        console.error("Lỗi:", err);
+      });
+
     onClose(); // đóng modal
   };
 
@@ -47,10 +74,16 @@ function LoginModalContent({ onClose }) {
         className="bg-white rounded-xl shadow-md w-[380px] p-6 relative animate-fade-in"
         onClick={(e) => e.stopPropagation()}
       >
-        <h2 className="text-center text-lg font-semibold mb-4">Đăng nhập WellCoin</h2>
+        <h2 className="text-center text-lg font-semibold mb-4">
+          Đăng nhập WellMarket
+        </h2>
 
         <button className="w-full text-gray-600 flex items-center justify-center border rounded-full py-2 hover:text-black hover:bg-pink-200 mb-2">
-          <img src="https://www.svgrepo.com/show/355037/google.svg" className="w-5 h-5 mr-2" alt="Google" />
+          <img
+            src="https://www.svgrepo.com/show/355037/google.svg"
+            className="w-5 h-5 mr-2"
+            alt="Google"
+          />
           Tiếp tục với Google
         </button>
 
@@ -65,13 +98,30 @@ function LoginModalContent({ onClose }) {
           placeholder="Số điện thoại"
           value={phone}
           onChange={(e) => setPhone(e.target.value)}
-          className={`w-full border rounded-lg p-2 focus:ring-2 mb-1 ${
-            error ? "border-red-500 focus:ring-red-300" : "border-gray-300 focus:ring-pink-200"
+          className={`w-full border rounded-lg p-2 focus:ring mb-1 ${
+            error
+              ? "border-red-500 focus:ring-red-300 "
+              : " focus:ring-pink-200 focus:shadow-lg focus:shadow-pink-200"
           }`}
         />
-        {error && <p className="text-red-500 text-sm mb-3 text-center">{error}</p>}
-
-        <button onClick={handleSubmit} className="w-full bg-pink-500 text-white rounded-lg py-2 font-medium hover:bg-pink-600">
+        {error && (
+          <p className="text-red-500 text-sm mb-3 text-center">{error}</p>
+        )}
+        <input
+          placeholder="Mật khẩu"
+          value={pswd}
+          onChange={(e) => setPassword(e.target.value)}
+          className="w-full border rounded-lg p-2 focus:ring-2 mb-1  border-gray-300 focus:ring-pink-200 
+                      focus:shadow-lg focus:shadow-pink-200 mt-2"
+          type="password"
+          maxLength={30}
+          required
+        />
+        {errorPwd && <p className="text-red-500 text-sm">{errorPwd}</p>}
+        <button
+          onClick={handleSubmit}
+          className="w-full mt-2 bg-pink-400 text-gray-500 rounded-lg py-2 font-medium hover:bg-pink-600 hover:text-black "
+        >
           Tiếp tục
         </button>
 
@@ -79,15 +129,27 @@ function LoginModalContent({ onClose }) {
           <hr className="flex-grow border-gray-300" />
           <span className="px-2 text-sm text-gray-500">
             Chưa có tài khoản?
-            <a href="#" className="hover:underline hover:text-pink-700 ml-2" onClick={handleRegisterClick}>Đăng ký</a>
+            <a
+              href="#"
+              className="hover:underline hover:text-pink-700 ml-2"
+              onClick={handleRegisterClick}
+            >
+              Đăng ký
+            </a>
           </span>
           <hr className="flex-grow border-gray-300" />
         </div>
 
         <div className="text-center text-xs text-gray-500 space-x-3 mt-4 flex justify-center">
-          <a href="#" className="hover:underline hover:text-pink-700">Quy chế hoạt động sàn</a>
-          <a href="#" className="hover:underline hover:text-pink-700">Chính sách bảo mật</a>
-          <a href="#" className="hover:underline hover:text-pink-700">Liên hệ hỗ trợ</a>
+          <a href="#" className="hover:underline hover:text-pink-700">
+            Quy chế hoạt động sàn
+          </a>
+          <a href="#" className="hover:underline hover:text-pink-700">
+            Chính sách bảo mật
+          </a>
+          <a href="#" className="hover:underline hover:text-pink-700">
+            Liên hệ hỗ trợ
+          </a>
         </div>
       </div>
     </div>
--- fe/src/components/Login/Register.jsx
+++ fe/src/components/Login/Register.jsx
@@ -1,51 +1,114 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
+import { LoginModal } from "./Login";
+import bcrypt from "bcryptjs";
+import axios from "axios";
+
+function checkPasswordConditions(password) {
+  return {
+    length: password.length >= 8 && password.length <= 30,
+    uppercase: /[A-Z]/.test(password),
+    lowercase: /[a-z]/.test(password),
+    number: /[0-9]/.test(password),
+    special: /[^A-Za-z0-9]/.test(password),
+  };
+}
 
 // ==========================================
 // Nội dung modal đăng ký
 // ==========================================
 function RegisterModalContent({ onClose }) {
-  const [form, setForm] = useState({
-    phone: "",
-    name: "",
-    password: "",
-    confirmPassword: "",
-    agree: false,
-  });
+  const [phone, setPhone] = useState("");
+  const [error, setPhoneError] = useState("");
 
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setForm((prev) => ({
-      ...prev,
-      [name]: type === "checkbox" ? checked : value,
-    }));
+  const [pswd, setPassword] = useState("");
+  const [errorPwd, setErrorPwd] = useState("");
+
+  const [name, setName] = useState("");
+  const [errorName, setErrorName] = useState("");
+
+  const [confirmPswd, setConfirmPswd] = useState("");
+  const [errorConfPass, setErrorConfPass] = useState("");
+
+  const [agree, setAgree] = useState(false);
+  const [errorAgree, setErrorAgree] = useState("");
+
+  const conditions = checkPasswordConditions(pswd);
+  const [showConditions, setShowConditions] = useState(false);
+
+  const validatePhone = (value) => {
+    const regex = /^0\d{9}$/;
+    if (!value) return "Vui lòng nhập số điện thoại";
+    if (!regex.test(value))
+      return "Số điện thoại phải gồm 10 chữ số và bắt đầu bằng 0";
+    return "";
   };
 
   const handleSubmit = () => {
-    if (!form.agree) {
-      alert("Vui lòng đồng ý với Điều Khoản Sử Dụng!");
+    let valid = true;
+
+    const phoneErr = validatePhone(phone);
+    if (phoneErr) {
+      setPhoneError(phoneErr);
       return;
-    }
-    if (form.password !== form.confirmPassword) {
-      alert("Mật khẩu nhập lại không khớp!");
-      return;
+    } else {
+      setPhoneError("");
     }
 
-    console.log("Thông tin đăng ký:", form);
+    if (!name) {
+      setErrorName("Vui lòng nhập họ và tên");
+      return;
+    } else {
+      setErrorName("");
+    }
+    const pwdErr = checkPasswordConditions(pswd);
+    if (Object.values(pwdErr).some((v) => v === false)) {
+      setErrorPwd("Mật khẩu ko hơp lê");
+      return;
+    } else {
+      setErrorPwd("");
+    }
+    console.log(pwdErr);
+    if (confirmPswd !== pswd) {
+      setErrorConfPass("Mật khẩu nhập lại không khớp");
+      return;
+    } else {
+      setErrorConfPass("");
+    }
+
+    if (!agree) {
+      setErrorAgree("Bạn phải đồng ý với Điều Khoản Sử Dụng");
+      return;
+    } else {
+      setErrorAgree("");
+    }
+
+    // Nếu tất cả hợp lệ
+
+    const hashedpswd = bcrypt.hashSync(pswd, 5);
+    console.log("Thông tin đăng ký:", phone, name, hashedpswd);
+    const data = {
+      Phone: phone,
+      Name: name,
+      Passord: hashedpswd,
+    };
+    console.log(data);
+    axios
+      .post("http://localhost:8000/login", data)
+      .then((res) => {
+        console.log("Server trả về:", res.data);
+      })
+      .catch((err) => {
+        console.error("Lỗi:", err);
+      });
+
     alert("Đăng ký thành công!");
     onClose();
-  };
-
-  // ✅ Đóng popup khi click ra ngoài
-  const handleOverlayClick = (e) => {
-    if (e.target === e.currentTarget) onClose();
+    LoginModal.show();
   };
 
   return (
-    <div
-      className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-[9999]"
-      onClick={handleOverlayClick}
-    >
+    <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-[9999]">
       <div
         className="bg-white rounded-xl shadow-md w-[400px] p-6 relative animate-fade-in"
         onClick={(e) => e.stopPropagation()}
@@ -59,44 +122,122 @@ function RegisterModalContent({ onClose }) {
             type="tel"
             name="phone"
             placeholder="Số điện thoại"
-            value={form.phone}
-            onChange={handleChange}
-            className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-pink-200"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+            className={`w-full border rounded-lg p-2 focus:ring mb-1 ${
+              error
+                ? "border-red-500 focus:ring-red-300 "
+                : " focus:ring-pink-200 focus:shadow-lg focus:shadow-pink-200"
+            }`}
           />
-
+          {error && (
+            <p className="text-red-500 text-sm mb-3 text-center">{error}</p>
+          )}
           <input
             type="text"
             name="name"
             placeholder="Họ và tên"
-            value={form.name}
-            onChange={handleChange}
-            className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-pink-200"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            maxLength={100}
+            className={`w-full border rounded-lg p-2 focus:ring mb-1 ${
+              errorName
+                ? "border-red-500 focus:ring-red-300 "
+                : " focus:ring-pink-200 focus:shadow-lg focus:shadow-pink-200"
+            }`}
           />
-
-          <input
-            type="password"
-            name="password"
-            placeholder="Mật khẩu"
-            value={form.password}
-            onChange={handleChange}
-            className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-pink-200"
-          />
+          {errorName && (
+            <p className="text-red-500 text-sm mb-3 text-center">{errorName}</p>
+          )}
+          <div>
+            <input
+              type="password"
+              name="password"
+              placeholder="Mật khẩu"
+              value={pswd}
+              onChange={(e) => setPassword(e.target.value)}
+              onFocus={() => {
+                setShowConditions(true), setErrorPwd("");
+              }}
+              onBlur={() => setShowConditions(false)}
+              className={`w-full border rounded-lg p-2 focus:ring mb-1 ${
+                errorPwd
+                  ? "border-red-500 focus:ring-red-300 "
+                  : " focus:ring-pink-200 focus:shadow-lg focus:shadow-pink-200"
+              }`}
+            />
+            {errorPwd && (
+              <p className="text-red-500 text-sm mb-3 text-center">
+                {errorPwd}
+              </p>
+            )}
+            {showConditions && (
+              <ul className="mt-2 text-sm">
+                <li
+                  className={
+                    conditions.length ? "text-green-600" : "text-red-600"
+                  }
+                >
+                  {conditions.length ? "✅" : "❌"} Độ dài từ 8–30 ký tự
+                </li>
+                <li
+                  className={
+                    conditions.uppercase ? "text-green-600" : "text-red-600"
+                  }
+                >
+                  {conditions.uppercase ? "✅" : "❌"} Có ít nhất một chữ hoa
+                  (A–Z)
+                </li>
+                <li
+                  className={
+                    conditions.lowercase ? "text-green-600" : "text-red-600"
+                  }
+                >
+                  {conditions.lowercase ? "✅" : "❌"} Có ít nhất một chữ thường
+                  (a–z)
+                </li>
+                <li
+                  className={
+                    conditions.number ? "text-green-600" : "text-red-600"
+                  }
+                >
+                  {conditions.number ? "✅" : "❌"} Có ít nhất một chữ số (0–9)
+                </li>
+                <li
+                  className={
+                    conditions.special ? "text-green-600" : "text-red-600"
+                  }
+                >
+                  {conditions.special ? "✅" : "❌"} Có ít nhất một ký tự đặc
+                  biệt (!@#$%^&*…)
+                </li>
+              </ul>
+            )}
+          </div>
 
           <input
             type="password"
             name="confirmPassword"
             placeholder="Nhập lại mật khẩu"
-            value={form.confirmPassword}
-            onChange={handleChange}
-            className="w-full border border-gray-300 rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-pink-200"
+            value={confirmPswd}
+            onChange={(e) => setConfirmPswd(e.target.value)}
+            className={`w-full border rounded-lg p-2 focus:ring mb-1 ${
+              errorConfPass
+                ? "border-red-500 focus:ring-red-300 "
+                : " focus:ring-pink-200 focus:shadow-lg focus:shadow-pink-200"
+            }`}
           />
-
+          {errorConfPass && (
+            <p className="text-red-500 text-sm mb-3 text-center">
+              {errorConfPass}
+            </p>
+          )}
           <label className="flex items-center text-sm text-gray-600">
             <input
               type="checkbox"
               name="agree"
-              checked={form.agree}
-              onChange={handleChange}
+              checked={agree}
+              onChange={(e) => setAgree(e.target.value)}
               className="mr-2 accent-pink-500"
             />
             Tôi đồng ý với{" "}
@@ -104,7 +245,11 @@ function RegisterModalContent({ onClose }) {
               Điều Khoản Sử Dụng
             </a>
           </label>
-
+          {errorAgree && (
+            <p className="text-red-500 text-sm mb-3 text-center">
+              {errorAgree}
+            </p>
+          )}
           <button
             onClick={handleSubmit}
             className="w-full bg-pink-500 text-white rounded-lg py-2 font-medium hover:bg-pink-600 mt-2"
@@ -115,7 +260,7 @@ function RegisterModalContent({ onClose }) {
 
         <button
           onClick={onClose}
-          className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
+          className="absolute font-semibold top-3 right-3 text-gray-500 hover:text-black hover:font-extrabold "
         >
           ✕
         </button>
